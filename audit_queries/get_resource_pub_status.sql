5844d7f43259a806eacacecc98e9a4e6e4d1342d
ucancallmealicia <adetelich@gmail.com>
2017-09-06 14:36:56 -0400

    Create get_resource_pub_status.sql

---
+++ audit_queries/get_resource_pub_status.sql
@@ -0,0 +1,9 @@
+#get resource-level restriction info
+SELECT publish AS resource_pub_status
+    , title AS resource_title
+    , ead_id AS EAD_ID
+FROM resource
+#filter by repo_id for sake of performance and repo-by-repo review
+WHERE repo_id = 12 #change for each repo
+#1 is published, 0 is unpublished
+AND publish = 1 #change for each pub status
