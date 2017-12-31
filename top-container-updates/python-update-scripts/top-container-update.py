3c4a80058a5a2471265260630a47a542dc23b07d
fordmadox <fordmadox@gmail.com>
2017-12-31 16:28:33 -0500

    Top container stuff (now functioning) (#5)
    * adding files used for container reports
    
    * string-join update
    
    * brevity is the soul of bit

--- top-container-updates/python-update-scripts/top-container-update.py
+++ top-container-updates/python-update-scripts/top-container-update.py
@@ -1,101 +1,80 @@
 """Script to update top container records in ArchivesSpace based on values in a csv input file.
-still need to move all of the login and logging settings to configuration files"""
-import getpass, requests, json, csv, datetime, logging
+still need to move all of the login and logging settings to configuration files
+questions:
+any way to get an accurate location start date from GFA?
+if not, should the location start date include a location note, mentioning that the value supplied is based on the date when the script was run?
+what else???
+"""
+import getpass, requests, json, csv, datetime, sys, logging
 
- 
 def login(api_url):
     username = getpass.getuser()
     check_username = input('Is your username ' + username + '?: ')
     if check_username.lower() not in ('y', 'yes', 'yep', 'you know it'):
         username = input('Please enter ArchivesSpace username:  ')
     password = getpass.getpass(prompt=username + ', please enter your ArchivesSpace Password: ', stream=None)
-    auth = requests.post(api_url+'/users/'+username+'/login?password='+password).json()
-    #if session object is returned then the login was successful; if not it failed.
-    if 'session' in auth:
-        session = auth["session"]
+    auth_request = requests.post(api_url+'/users/'+username+'/login?password='+password).json()
+    if 'session' in auth_request:
+        session = auth_request['session']
         authorization_token = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
         print('Login successful!')
         return (authorization_token)
     else:
         print('Ooops! Try logging in again.')
-        return
+        sys.exit()
  
 def open_csv():
     '''This function opens a csv file in DictReader mode'''
     input_csv = input('Please enter path to CSV: ')
     file = open(input_csv, 'r', encoding='utf-8')
     return csv.DictReader(file)
- 
-def get_container_profile_URIs(api_url, headers):
-    container_profiles = []
-    print('Retrieving container profile data. This may take a moment')
-    id_list = requests.get(api_url + '/container_profiles?all_ids=True', headers=authorization_token).json()
-    for id in id_list:
-        profile_dict = {}
-        id_json = requests.get(api_url + '/container_profiles/' + str(id), headers=authorization_token).json()
-        for key, value in id_json.items():
-             if key == 'name':
-                 profile_dict['name'] = value
-             if key == 'uri':
-                 profile_dict['uri'] = value
-             container_profiles.append(profile_dict)
-    print('Containers retrieved!')
-    return [container_profiles]
-     
+      
 def update_top_containers(api_url, headers, container_locations=None):
     csv = open_csv()
+    default_date = str(datetime.date.today())
     for row in csv:
-        print(row)
         try:
-            record_uri = row['top-container-URI']
-            container_location = row['aspace-location-URI-to-post']
+            record_uri = row.get("top-container-URI")
+            new_barcode = row.get("voyager-barcode")
+            container_location = row.get("aspace-location-URI-to-post")
+            location_date_to_post = row.get("start_date", default_date)
+            if location_date_to_post == '':
+                location_date_to_post = default_date
+            
             record_json = requests.get(api_url + record_uri, headers=authorization_token).json()
-            for key, value in row.items():
-                # use the start_date value when creating a new location association; if a start_date is not present, default to today's date
-                location_date_to_post = value if (key == 'start_date' and value != '') else str(datetime.date.today())
+            logging.info(record_uri + ' Original JSON: ' + json.dumps(record_json))
+            # check to see if the container_locations list is empty.
+            if not record_json["container_locations"] and container_location != '':
+                record_json["container_locations"].append({'start_date':location_date_to_post, 'ref':container_location, 'status':'current'})
+            else:
+            # this next bit is all that's needed to update existing location information, and for now I'm assuming that there's only 1 location.
+                for location in record_json["container_locations"]:
+                    current_location = location.get('ref')
+                    if container_location != '':
+                        location['ref'] = container_location
+                    if container_location != current_location:
+                        location['start_date'] = location_date_to_post
+
+            if new_barcode:
+                record_json['barcode'] = new_barcode
                 
-                # check to see if the container_locations list is empty, since ASpace adds that key even if there are no locations assigned.
-                # i don't want to check for the empty array like this, but when i tried other methods, it always evaluated to True.  so, going with len() for now
-                if len(record_json['container_locations']) == 0 and container_location != '':
-                    record_json['container_locations'].append({'start_date':location_date_to_post, 'ref':container_location, 'status':'current'})
-                else:
-                    # this next bit is all that's needed to update existing location information
-                    for member in record_json['container_locations']:
-                        if key == 'aspace-location-URI-to-post' and value != '':
-                            member['ref'] = value
-                        if key == 'location_start_date' and value != '':
-                            member['start_date'] = value
-
-           
-                '''
-                #Look up container profile name and add uri if found
-                if key == 'container_profile':
-                    for keyval in cp_list:
-                        if value in keyval.values():
-                            record_json['container_profile'] = {'ref': keyval.get('uri')}
-                '''
-
-                if key == 'voyager-barcode':
-                    print('barcode is ' + value)
-                    record_json['barcode'] = value
-                    
             record_data = json.dumps(record_json)
             record_update = requests.post(api_url + record_uri, headers=authorization_token, data=record_data).json()
             print(record_update)
             logging.info('%s successfully updated', record_uri)
-        except:
+            logging.info(record_uri + ' Updated JSON: ' + record_data)
+            logging.info('\n')
+        
+        except Exception as ex:
             print('Skipping invalid row')
-            logging.warning('Skipping invalid row for %s', record_uri)
+            logging.warning('Skipping invalid row for %s. Exception: %s\n', record_uri, ex)
             continue
 
 def main():
-    logging.info('Started')
-    '''i'm not updating container_profiles in this first pass, so this section has been removed from the script for now'''
-    # container_profiles = get_container_profile_URIs(api_url, authorization_token)
+    logging.info('Started updates in %s', api_url)
     update_top_containers(api_url, authorization_token)
-    logging.info('Finished')
-    input("Press enter to exit...")
-
+    logging.info('Finished updates in %s', api_url)
+    input("Press enter to exit... ")
 
 if __name__ == '__main__':
     logging.basicConfig(filename='container-updates.log', level=logging.INFO, format='%(asctime)s %(message)s')
