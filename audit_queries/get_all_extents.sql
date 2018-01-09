56b6f57d8f62b7eb9bd4c657c4483688da7779c7
ucancallmealicia <adetelich@gmail.com>
2018-01-09 11:47:51 -0500

    add repo_id, extent_id, change order

--- audit_queries/get_all_extents.sql
+++ audit_queries/get_all_extents.sql
@@ -1,76 +1,76 @@
-SELECT ev.value as portion
-	, e.number
+SELECT CONCAT('/repositories/', resource.repo_id, '/resources/', resource.id) AS uri
 	, ev2.value as extent_type
-    , e.container_summary
-    , e.physical_details
-    , e.dimensions
-    , resource.title as title
-    , CONCAT('/repositories/', resource.repo_id, '/resources/', resource.id) AS uri
+    	, e.container_summary
+    	, resource.title as title
+	, ev.value as portion
+	, e.number
+    	, resource.repo_id as repo_id
+    	, e.id as extent_id
 FROM resource
 JOIN extent e on resource.id = e.resource_id
 LEFT JOIN enumeration_value ev on ev.id = e.portion_id
 LEFT JOIN enumeration_value ev2 on ev2.id = e.extent_type_id
 UNION ALL
-SELECT ev.value as portion
-	, e.number
+SELECT CONCAT('/repositories/', accession.repo_id, '/accessions/', accession.id) AS uri
 	, ev2.value as extent_type
-    , e.container_summary
-    , e.physical_details
-    , e.dimensions
-    , accession.title as title
-    , CONCAT('/repositories/', accession.repo_id, '/accessions/', accession.id) AS uri
+    	, e.container_summary
+    	, accession.title as title
+	, ev.value as portion
+	, e.number 
+    	, accession.repo_id as repo_id
+    	, e.id as extent_id
 FROM accession
 JOIN extent e on accession.id = e.accession_id
 LEFT JOIN enumeration_value ev on ev.id = e.portion_id
 LEFT JOIN enumeration_value ev2 on ev2.id = e.extent_type_id
 UNION ALL
-SELECT ev.value as portion
-	, e.number
+SELECT CONCAT('/repositories/', ao.repo_id, '/archival_objects/', ao.id) AS uri
 	, ev2.value as extent_type
-    , e.container_summary
-    , e.physical_details
-    , e.dimensions
-    , ao.title as title
-    , CONCAT('/repositories/', ao.repo_id, '/archival_objects/', ao.id) AS uri
+    	, e.container_summary
+    	, ao.title as title
+	, ev.value as portion
+	, e.number
+    	, ao.repo_id as repo_id
+    	, e.id as extent_id
 FROM archival_object ao
 JOIN extent e on ao.id = e.archival_object_id
 LEFT JOIN enumeration_value ev on ev.id = e.portion_id
 LEFT JOIN enumeration_value ev2 on ev2.id = e.extent_type_id
 UNION ALL
-SELECT ev.value as portion
-	, e.number
+SELECT CONCAT('/repositories/', do.repo_id, '/digital_objects/', do.id) AS uri
 	, ev2.value as extent_type
-    , e.container_summary
-    , e.physical_details
-    , e.dimensions
-    , do.title as title
-    , CONCAT('/repositories/', do.repo_id, '/digital_objects/', do.id) AS uri
+    	, e.container_summary
+    	, do.title as title
+	, ev.value as portion
+	, e.number
+    	, do.repo_id as repo_id
+    	, e.id as extent_id
 FROM digital_object do
 JOIN extent e on do.id = e.digital_object_id
 LEFT JOIN enumeration_value ev on ev.id = e.portion_id
 LEFT JOIN enumeration_value ev2 on ev2.id = e.extent_type_id
 UNION ALL
-SELECT ev.value as portion
-	, e.number
+SELECT CONCAT('/repositories/', doc.repo_id, '/digital_object_components/', doc.id) AS uri
 	, ev2.value as extent_type
-    , e.container_summary
-    , e.physical_details
-    , e.dimensions
-    , doc.title as title
-    , CONCAT('/repositories/', doc.repo_id, '/digital_object_components/', doc.id) AS uri
+    	, e.container_summary
+    	, doc.title as title
+	, ev.value as portion
+	, e.number
+    	, doc.repo_id as repo_id
+    	, e.id as extent_id
 FROM digital_object_component doc
 JOIN extent e on doc.id = e.digital_object_component_id
 LEFT JOIN enumeration_value ev on ev.id = e.portion_id
 LEFT JOIN enumeration_value ev2 on ev2.id = e.extent_type_id
 UNION ALL
-SELECT ev.value as portion
-	, e.number
+SELECT NULL as uri
 	, ev2.value as extent_type
-    , e.container_summary
-    , e.physical_details
-    , e.dimensions
-    , deaccession.id as title
-    , NULL as uri
+    	, e.container_summary
+    	, deaccession.id as title
+	, ev.value as portion
+	, e.number
+    	, NULL as repo_id
+    	, e.id as extent_id
 FROM deaccession
 JOIN extent e on deaccession.id = e.deaccession_id
 LEFT JOIN enumeration_value ev on ev.id = e.portion_id
